f8022854fe4c4cf7a271c80b890d040bd41a2a23
hotlikesauce-designs <146861584+hotlikesauce-designs@users.noreply.github.com>
2023-10-18 14:12:34 +0000

    dynamic

--- app.rb
+++ app.rb
@@ -52,3 +52,18 @@ get("/dice/5/4") do
 
   erb(:five_four)
 end
+
+get("/dice/:elephant/:zebra") do
+  
+  @number_of_dice = @params.fetch("elephant").to_i
+  @how_many_sides = @params.fetch("zebra").to_i
+  
+  @rolls =[]
+
+  @number_of_dice.times do
+    die = rand(1..@how_many_sides)
+    @rolls.push(die)
+  end
+
+  erb(:flexible)
+end
